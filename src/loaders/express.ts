6a1c7c82457dab889c598a1bf0bf5af62be6fee4
Park hanjun <park.hanjun00@gmail.com>
2020-12-27 00:37:25 +0900

    delete swagger

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -4,9 +4,6 @@ import * as cors from 'cors';
 import * as path from 'path';
 import * as cookieParser from 'cookie-parser';
 import * as logger from 'morgan';
-import * as swaggerJsdoc from 'swagger-jsdoc';
-import * as swaggerUi from 'swagger-ui-express';
-import swaggerOptions from '../config/swaggerOIptions';
 
 const initExpressLoaders = (app: express.Application): express.Application => {
     app.use(cors());
@@ -17,12 +14,6 @@ const initExpressLoaders = (app: express.Application): express.Application => {
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
 
-    const specs = swaggerJsdoc(swaggerOptions);
-    app.use(
-        '/docs',
-        swaggerUi.serve,
-        swaggerUi.setup(specs, { explorer: true })
-    );
     return app;
 };
 
